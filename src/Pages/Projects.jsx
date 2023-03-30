74fb033b2142547b0a3b740da64e627fe2b7bdf9
Ousman Touray <otouray14@gmail.com>
2023-03-30 11:39:42 -0400

    still working on projects

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,14 +1,14 @@
 import EachProject from "../Components/EachProject";
-import MovieAPI from '../assets/MovieAPI.png'
+import MovieSearch from "../assets/MovieSearch.png"
 import "../Styles/Project.css"
+
 
 export default function Projects(){
     return(
         <div className="projects">
             <h1>My Personal Projects</h1>
             <div className="projectList">
-                <EachProject name="Movie Search" image={MovieAPI}/>
-                <EachProject />
+                <EachProject name="Movie Search" image={MovieSearch}/>
             </div>
         </div>
     )
